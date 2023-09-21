6d18ff983c22bcf3e0b568e680c36edae2404fb4
Rael Wanjala <raelnwanjala@gmail.com>
2023-09-20 19:13:08 -0700

    Added the axios npm packageto handle the weatherstack api,added routes to weatherRoutes.js

--- controllers/api/weatherRoutes.js
+++ controllers/api/weatherRoutes.js
@@ -1,3 +1,21 @@
-const router = require('express').Router();
+const router = require("express").Router();
+const axios = require("axios");
+
+const params = {
+  access_key: "d9811cfed9b9febb1511db8f165bf5d2",
+  query: "New York",
+};
+
+axios
+  .get("https://api.weatherstack.com/current", { params })
+  .then((response) => {
+    const apiResponse = response.data;
+    console.log(
+      `Current temperature in ${apiResponse.location.name} is ${apiResponse.current.temperature}℃`
+    );
+  })
+  .catch((error) => {
+    console.log(error);
+  });
 
 module.exports = router;
