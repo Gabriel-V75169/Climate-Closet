b94f86999736d443ca5bf84a2b972c7b8e004efa
Rael Wanjala <raelnwanjala@gmail.com>
2023-09-21 19:07:41 -0700

    Added cadded code to weather.js, moved home-routes to controllers

---
+++ models/weather.js
@@ -0,0 +1,35 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+
+class weather extends Model {}
+
+weather.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    location: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "user",
+        key: "id",
+      },
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "weather",
+  }
+);
+
+module.exports = weather;
