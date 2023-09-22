3de0648b3d21c3bc3e6fac11ce293650abcde4ac
Taylor Rozier <roziertaylor@gmail.com>
2023-09-21 18:40:09 -0700

    model

--- controllers/index.js
+++ controllers/index.js
@@ -1,8 +1,5 @@
 const router = require('express').Router();
-
 const apiRoutes = require('./api');
-const homeRoutes = require('./homeRoutes');
-// const userRoutes = require('./api/userRoutes')
 
 router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
