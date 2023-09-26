a2615699f13109034aaa25155ad4f0f342bf6877
Rael Wanjala <raelnwanjala@gmail.com>
2023-09-25 21:06:34 -0700

    Added api route to shopping

--- controllers/api/shoppingRoutes.js
+++ controllers/api/shoppingRoutes.js
@@ -1,4 +1,25 @@
-const router = require('express').Router();
-const { Shopping } = require('../../models');
+const router = require("express").Router();
+const {User, Customize } = require("../../models");
+require("dotenv").config(); // Load environment variables from .env file
+const withAuth = require("")
+
+const eBay = require("ebay-node-api");
+
+const appId = process.env.EBAY_APP_ID;
+const certId = process.env.EBAY_CERT_ID;
+const devId = process.env.EBAY_DEV_ID;
+
+const ebay = new eBay({
+  appId,
+  certId,
+  devId,
+});
+
+router.get("/" ,withAuth, async (req,res) =>{
+   try {
+    //retrieve all user id
+    const UserId = req.user.id; 
+   }
+})
 
 module.exports = router;
