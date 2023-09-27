953eae532ae3e23614eb24ee42a53b9a6f04cd54
Rael Wanjala <raelnwanjala@gmail.com>
2023-09-27 14:42:58 -0700

    Added models for product and category, added seeds folder for category and products

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,91 +1,89 @@
-const router = require('express').Router();
-const { Customize, User } = require('../models');
-const withAuth = require('../utils/auth')
+const router = require("express").Router();
+const { Customize, User } = require("../models");
+const withAuth = require("../utils/auth");
 
-router.get('/', async (req, res) => {
+router.get("/", async (req, res) => {
   try {
     // Get all customize requests and JOIN with user data
     const customData = await Customize.findAll({
       include: [
         {
           model: User,
-          attributes: ['name'],
-        },
-        {
-          model: Customize,
-          attributes: ['location','gender','style']
+          attributes: ["name"],
         },
       ],
     });
 
     // Serialize data so the template can read it
-    const customize = customData.map((customize) => customize.get({ plain: true }));
+    const customize = customData.map((customize) =>
+      customize.get({ plain: true })
+    );
 
     // Pass serialized data and session flag into template
-    res.render('home', { 
-      customize, 
-      logged_in: req.session.logged_in 
+    res.render("home", {
+      customize,
+      logged_in: req.session.logged_in,
     });
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
-router.get('/customize/:id', async (req, res) => {
+router.get("/customize/:id", async (req, res) => {
   try {
     const customData = await Customize.findAll({
       include: [
         {
           model: User,
-          attributes: ['name'],
+          attributes: ["name"],
         },
         {
           model: Customize,
-          attributes: ['location','gender','style']
+          attributes: ["location", "gender", "style"],
         },
       ],
     });
 
     const search = customData.get({ plain: true });
 
-    res.render('home', {
+    res.render("home", {
       ...search,
-      logged_in: req.session.logged_in
+      logged_in: req.session.logged_in,
     });
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
-router.get('/login', (req, res) => {
-    if (req.session.loggedIn) {
-      res.redirect('/profile');
-      return;
-    }
-    res.render('login');
-  });
+router.get("/login", (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect("/profile");
+    return;
+  }
+  res.render("login");
+});
 
-router.get('/profile', withAuth, async (req, res) => {
-    try {
-      // Find the logged in user based on the session ID
-      const userData = await User.findByPk(req.session.user_id, {
-        attributes: { exclude: ['password'] },
-        include: [{ model: Customize }],
-      });
-  
-      const user = userData.get({ plain: true });
-  
-      res.render('profile', {
-        ...user,
-        logged_in: true
-      });
-    } catch (err) {
-      res.status(500).json(err);
-    } 
-  });
+router.get("/profile", withAuth, async (req, res) => {
+  try {
+    // Find the logged in user based on the session ID
+    const userData = await User.findByPk(req.session.user_id, {
+      attributes: { exclude: ["password"] },
+      include: [{ model: Customize }],
+    });
 
-router.get('/customize', withAuth, (req,res) =>{
-  res.render('customize');
-})
+    const user = userData.get({ plain: true });
 
-  module.exports = router;
+    res.render("profile", {
+      ...user,
+      logged_in: true,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get("/customize", withAuth, (req, res) => {
+  res.render("customize");
+});
+
+module.exports = router;
