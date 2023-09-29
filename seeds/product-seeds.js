843ab76148b16439ee144fd90cb4cef9798cd875
RozierT <135091431+RozierT@users.noreply.github.com>
2023-09-28 17:15:04 -0700

    corrected syntax in seed file

--- seeds/product-seeds.js
+++ seeds/product-seeds.js
@@ -10,7 +10,7 @@
     season: "summer",
     gender: "male",
     image_url:
-      "https://res.cloudinary.com/dxedmyohg/image/upload/v1695914745/summer_male_tshirt_a06iw2.jpg",
+      "https://res.cloudinary.com/dxedmyohg/image/upload/v1695914745/summer_male_tshirt_a06iw2.jpg"
     //category_id: 1, // ID 1 for male summer items
   },
   {
@@ -20,7 +20,7 @@
     season: "summer",
     gender: "male",
     image_url:
-      "https://res.cloudinary.com/dxedmyohg/image/upload/v1695914609/male_summer_shorta_cujxpf.avif",
+      "https://res.cloudinary.com/dxedmyohg/image/upload/v1695914609/male_summer_shorta_cujxpf.avif"
     //category_id: 1,
   },
   {
@@ -30,7 +30,7 @@
     season: "winter",
     gender: "male",
     image_url:
-      "https://res.cloudinary.com/dxedmyohg/image/upload/v1695915263/male_winter_jacket_qqg9qe.jpg",
+      "https://res.cloudinary.com/dxedmyohg/image/upload/v1695915263/male_winter_jacket_qqg9qe.jpg"
     //category_id: 2, // category with ID 2 for male winter items
   },
   {
@@ -40,7 +40,7 @@
     season: "winter",
     gender: "male",
     image_url:
-      "https://res.cloudinary.com/dxedmyohg/image/upload/v1695915268/winter_male_pants_blnt9p.webp",
+      "https://res.cloudinary.com/dxedmyohg/image/upload/v1695915268/winter_male_pants_blnt9p.webp"
     //category_id: 2,
   },
   {
@@ -50,7 +50,7 @@
     season: "fall",
     gender: "male",
     image_url:
-      "https://res.cloudinary.com/dxedmyohg/image/upload/v1695914924/male_fall_sweater_gwdv0d.webp",
+      "https://res.cloudinary.com/dxedmyohg/image/upload/v1695914924/male_fall_sweater_gwdv0d.webp"
     //category_id: 3, // category with ID 3 for male fall items
   },
 
@@ -62,7 +62,7 @@
     season: "summer",
     gender: "female",
     image_url:
-      "https://res.cloudinary.com/dxedmyohg/image/upload/v1695915269/women_summer_dress_h0qh8o.webp",
+      "https://res.cloudinary.com/dxedmyohg/image/upload/v1695915269/women_summer_dress_h0qh8o.webp"
     //category_id: 4, //category with ID 4 for female summer items
   },
   {
@@ -72,7 +72,7 @@
     season: "summer",
     gender: "female",
     image_url:
-      "https://res.cloudinary.com/dxedmyohg/image/upload/v1695915265/summer_women_sandal_fjtskp.webp",
+      "https://res.cloudinary.com/dxedmyohg/image/upload/v1695915265/summer_women_sandal_fjtskp.webp"
     //category_id: 4,
   },
   {
@@ -82,7 +82,7 @@
     season: "winter",
     gender: "female",
     image_url:
-      "https://res.cloudinary.com/dxedmyohg/image/upload/v1695915266/winter_boots_women_fywmse.webp",
+      "https://res.cloudinary.com/dxedmyohg/image/upload/v1695915266/winter_boots_women_fywmse.webp"
     // category_id: 5, //  ID 5 for female winter items
   },
   {
@@ -92,7 +92,7 @@
     season: "winter",
     gender: "female",
     image_url:
-      "https://res.cloudinary.com/dxedmyohg/image/upload/v1695915266/winter_boots_women_fywmse.webp",
+      "https://res.cloudinary.com/dxedmyohg/image/upload/v1695915266/winter_boots_women_fywmse.webp"
     //category_id: 5,
   },
   {
@@ -102,9 +102,9 @@
     season: "fall",
     gender: "female",
     image_url:
-      "https://res.cloudinary.com/dxedmyohg/image/upload/v1695915261/fall_cardigan_p22qva.webp",
+      "https://res.cloudinary.com/dxedmyohg/image/upload/v1695915261/fall_cardigan_p22qva.webp"
     // category_id: 6, // category with ID 6 for female fall items
-  },
+  }
 ];
 // const seedProducts = async () => {
 //   try {
